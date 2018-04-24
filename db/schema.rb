24b0a79aa72b34e97178b157020c0ca6a3c5e090
SnIzoK <sashko-tvirkun@mail.ru>
2018-04-24 17:49:31 +0300

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -138,9 +138,6 @@ ActiveRecord::Schema.define(version: 20180423161804) do
   end
 
   create_table "sertificates", force: :cascade do |t|
-    t.string  "name"
-    t.string  "image"
-    t.boolean "show"
   end
 
   create_table "users", force: :cascade do |t|
