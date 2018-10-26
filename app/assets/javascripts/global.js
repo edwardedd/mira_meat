17a410029938b1027a03b3eeccd1a42630a20c16
YuraHodan <yura.hodan@gmail.com>
2018-10-26 14:12:22 +0300

    add google atag

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -2,3 +2,13 @@ $document = $(document)
 
 is_touch_screen = 'ontouchstart' in document.documentElement;
 width = window.innerWidth;
+
+
+///google gtag
+
+
+window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+
+ gtag('config', 'UA-119552559-1');
